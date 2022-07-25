a5b5784470d4c0a22e223ff9af1f092dfc21fc20
JasonMan34 <JasonErlMan@Gmail.com>
2022-07-25 15:53:42 +0300

    Works

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,36 +1,45 @@
 import { corrupt } from "hamming-corruptor";
 import { decode } from "hamming-decoder";
 import { encode_8_4 } from "hamming-encoder";
+import { downloadFile, FileName } from "./utils";
 
 interface File {
+  name: string;
   content: Uint8Array;
   encoded: Uint8Array;
   corrupted: Uint8Array;
   decodedFixed: Uint8Array;
   decodedUnfixed: Uint8Array;
-  text: String;
+  text: string;
 }
 
 let file: File | undefined;
 
 const fileInput = document.getElementById("file") as HTMLInputElement;
-const encodeBtn = document.getElementById("encode") as HTMLButtonElement;
-const decodeBtn = document.getElementById("decode") as HTMLButtonElement;
-const corruptBtn = document.getElementById("corrupt") as HTMLButtonElement;
+const getEncoded = document.getElementById("get_encoded");
+const getCorruptedEncoded = document.getElementById("get_corrupted_encoded");
+const getCorrupted = document.getElementById("get_corrupted");
+const getRecovered = document.getElementById("get_recovered");
+
+if (!getEncoded || !getCorruptedEncoded || !getCorrupted || !getRecovered) {
+  throw new Error("Script load failed, missing elements in the HTML tree");
+}
 
 fileInput.onchange = async () => {
-  let buffer = await fileInput.files?.[0].arrayBuffer();
-  if (buffer) {
-    let content = new Uint8Array(buffer);
-    let encoded = encode_8_4(content);
-    let corrupted = corrupt(encoded);
-    let decodedFixed = decode(corrupted, true);
-    let decodedUnfixed = decode(corrupted, false);
-    let text = new TextDecoder().decode(buffer);
+  const inputFile = fileInput.files?.[0];
+  if (inputFile) {
+    const buffer = await inputFile.arrayBuffer();
+    const content = new Uint8Array(buffer);
+    const encoded = encode_8_4(content);
+    const corrupted = corrupt(encoded);
+    const decodedFixed = decode(corrupted, true);
+    const decodedUnfixed = decode(corrupted, false);
+    const text = new TextDecoder().decode(buffer);
 
-    console.log(text);
+    console.log(inputFile.name);
 
     file = {
+      name: fileInput.value,
       content,
       encoded,
       corrupted,
@@ -39,4 +48,36 @@ fileInput.onchange = async () => {
       text,
     };
   }
+};
+
+getEncoded.onclick = () => {
+  if (!file) return;
+
+  const fileName = new FileName(file.name);
+  fileName.appendName(`.${fileName.extension}`).setExtension("hamming");
+  downloadFile(fileName.getFileName(), file.encoded);
+};
+
+getCorruptedEncoded.onclick = () => {
+  if (!file) return;
+
+  const fileName = new FileName(file.name);
+  fileName
+    .appendName(`_corrupted.${fileName.extension}`)
+    .setExtension("hamming");
+  downloadFile(fileName.getFileName(), file.corrupted);
+};
+
+getCorrupted.onclick = () => {
+  if (!file) return;
+
+  const fileName = new FileName(file.name).appendName("_corrupted");
+  downloadFile(fileName.getFileName(), file.decodedUnfixed);
+};
+
+getRecovered.onclick = () => {
+  if (!file) return;
+
+  const fileName = new FileName(file.name).appendName("_recovered");
+  downloadFile(fileName.getFileName(), file.decodedFixed);
 };
---
+++ www/src/utils.ts
@@ -0,0 +1,39 @@
+export const downloadFile = (name: string, content: Uint8Array) => {
+  const element = document.createElement("a");
+  document.body.appendChild(element);
+  element.setAttribute("style", "display: none");
+
+  const blob = new Blob([content], { type: "octet/stream" });
+  const url = window.URL.createObjectURL(blob);
+  element.href = url;
+  element.download = name;
+  element.click();
+  window.URL.revokeObjectURL(url);
+
+  document.body.removeChild(element);
+};
+
+export class FileName {
+  name: string;
+  extension: string;
+
+  constructor(name: string) {
+    const extensionIndex = name.lastIndexOf(".");
+    this.name = name.substring(0, extensionIndex);
+    this.extension = name.substring(extensionIndex + 1);
+  }
+
+  appendName(str: string) {
+    this.name += str;
+    return this;
+  }
+
+  setExtension(ext: string) {
+    this.extension = ext;
+    return this;
+  }
+
+  getFileName() {
+    return this.name + "." + this.extension;
+  }
+}
