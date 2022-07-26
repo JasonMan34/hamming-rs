5f96c179002db358ad9ba8de1e4d58ca51b0b74e
JasonMan34 <JasonErlMan@Gmail.com>
2022-07-26 16:27:36 +0300

    I'm stupid

--- www/src/index.ts
+++ www/src/index.ts
@@ -7,9 +7,6 @@ import { downloadFile, FileName, isText } from "./utils";
 // Small hack to prevent bad first render
 document.body.style.display = "";
 
-// work?
-const a = 1;
-
 interface File {
   name: string;
   content: Uint8Array;
